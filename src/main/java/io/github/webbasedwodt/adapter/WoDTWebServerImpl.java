8f5199ffa2d142e5e11c54b04d4dbbfae1ed3fa6
Andrea Giulianelli <andrygiulio@gmail.com>
2023-12-19 18:07:29 +0100

    feat: implement wodt web server

---
+++ src/main/java/io/github/webbasedwodt/adapter/WoDTWebServerImpl.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2023. Andrea Giulianelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.webbasedwodt.adapter;
+
+import io.github.webbasedwodt.application.component.DTDManagerReader;
+import io.github.webbasedwodt.application.component.DTKGEngine;
+import io.github.webbasedwodt.application.component.WoDTWebServer;
+import io.javalin.Javalin;
+
+import java.util.function.BiFunction;
+
+/**
+ * This class implement the WoDT Web server that host the WoDT Digital Twin Interface component
+ * of the Abstract Architecture.
+ */
+public final class WoDTWebServerImpl implements WoDTWebServer {
+    private final int portNumber;
+    private final WoDTDigitalTwinInterfaceControllerImpl wodtDigitalTwinInterfaceController;
+
+    /**
+     * Default constructor.
+     * @param portNumber the port number where to expose the API
+     * @param dtkgEngine the DTKGEngine
+     * @param dtdManager the DTDManager
+     * @param actionHandler handler of action invocation
+     */
+    public WoDTWebServerImpl(
+            final int portNumber,
+            final DTKGEngine dtkgEngine,
+            final DTDManagerReader dtdManager,
+            final BiFunction<String, String, Boolean> actionHandler
+    ) {
+        this.portNumber = portNumber;
+        this.wodtDigitalTwinInterfaceController = new WoDTDigitalTwinInterfaceControllerImpl(
+                dtkgEngine, dtdManager, actionHandler);
+        dtkgEngine.addDTKGObserver(this.wodtDigitalTwinInterfaceController::notifyNewDTKG);
+    }
+
+    @Override
+    public void start() {
+        final Javalin app = Javalin.create().start(this.portNumber);
+        this.wodtDigitalTwinInterfaceController.registerRoutes(app);
+    }
+}
